e2c774eb4f4dc73850f934d9444fc64075c87961
Niklas Voss <nvoss@google.com>
2022-08-02 11:03:09 +0200

    Construct URI to connect to postgres as well

--- models/index.ts
+++ models/index.ts
@@ -5,6 +5,19 @@ import { Sequelize } from 'sequelize-typescript';
 //       in our API we are not directly affected:
 //       https://github.com/sequelize/sequelize-typescript/issues/778
 
+let uri = process.env.DB_URI || "";
+if (uri === "") {
+    const dialect = process.env.DB_DIALECT || "";
+    const username = process.env.DB_USERNAME || "";
+    const password = process.env.DB_PASSWORD || "";
+    const host = process.env.DB_HOST || "";
+    const port = process.env.DB_PORT || "";
+    const database = process.env.DB_DATABASE || "";
+    if (dialect !== "") { // A dialect is specifed and uri not, so let's construct it ourselves
+        uri = dialect + "://" + username + (password !== "" ? ":" + password : "") + "@" + host + (port !== "" ? ":" + port : "") + "/" + database;
+    }
+}
+
 const sequelize = new Sequelize(process.env.DB_URI || "", {
     logging: process.env.NODE_ENV === 'production' ? false : console.log
 });
